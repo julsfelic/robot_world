d46affd7b21ee68b90030cbeec75c30fbf784526
Julian Feliciano <julsfelic@gmail.com>
2016-02-02 18:34:22 -0700

    Create Models for Robot and RobotManager along with YAML database

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,13 +1,25 @@
+require "models/robot_manager"
+
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
     # grab robots from RobotManager model
-    @robots = ["Robot 1", "Robot 2", "Robot 3"]
+    @robots = robot_manager.all
     erb :index
   end
 
   get '/robots/new' do
     erb :new
+  end
+
+  post '/robots' do
+    robot_manager.create(params[:robot])
+    redirect '/robots'
+  end
+
+  def robot_manager
+    database = YAML::Store.new('db/robot_manager')
+    @robot_manager ||= RobotManager.new(database)
   end
 end
---
+++ app/models/robot_manager.rb
@@ -0,0 +1,38 @@
+require 'yaml/store'
+require_relative 'robot'
+
+class RobotManager
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(robot)
+    database.transaction do
+      database['robots'] ||= []
+      database['total']  ||= 0
+      database['total']  += 1
+      database['robots'] << {
+        "id"         => database['total'],
+        "name"       => robot[:name],
+        "city"       => robot[:city],
+        "state"      => robot[:state],
+        "avatar"     => robot[:avatar],
+        "birthday"   => robot[:birthday],
+        "date_hired" => robot[:date_hired],
+        "department" => robot[:department]
+      }
+    end
+  end
+
+  def raw_robots
+    database.transaction do
+      database['robots'] || []
+    end
+  end
+
+  def all
+    raw_robots.map { |raw_robot| Robot.new(raw_robot) }
+  end
+end
